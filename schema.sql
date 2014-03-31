e8498edd2e0c10d7ceb71c28f2498a4ddfc066be
Vitalij <sounder.vitalij@gmail.com>
2014-03-31 11:55:50 +0300

    Initial project setup

---
+++ schema.sql
@@ -0,0 +1,14 @@
+drop table if exists users;
+create table users (
+  id integer primary key autoincrement,
+  username text not null,
+  password text not null
+);
+
+drop table if exists messages;
+create table messages (
+  id integer primary key autoincrement,
+  sender integer not null,
+  receiver integer not null,
+  Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
